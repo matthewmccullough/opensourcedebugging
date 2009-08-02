fcf45b1c9d5a980934ac7b07bff936144763a7ce
Matthew McCullough <matthewm@ambientideas.com>
2009-08-02 15:40:55 -0600

    Adding javap example script

--- javap-testapp/src/main/java/com/ambientideas/App.java
+++ javap-testapp/src/main/java/com/ambientideas/App.java
@@ -8,10 +8,17 @@ public class App implements java.io.Serializable
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      System.out.println( "Hello World!" );
+    }
+    
+    private static void doSomethingInternal() {
+      System.out.println( "Internals!" );
     }
     
     //Bean properties
     String name;
     Integer age;
+    
+    //Private properties
+    private String spouseName;
 }
